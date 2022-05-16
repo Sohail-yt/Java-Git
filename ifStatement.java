ee643546e2237c0b7bddb57498f700c46cefa70a
Shaik Mohammad Sohail <mohammadsohailyt@gmail.com>
2022-05-16 23:47:49 +0530

    commit

---
+++ ifStatement.java
@@ -0,0 +1,16 @@
+class ifStatement {
+     public static void main(String args[]) {
+          int a=45;
+          int b=65;
+          int c=95;
+          if(a>b & a>c){
+               System.out.printf("a=%d is bigger",b);
+          }
+          else if(b>a & b>c){
+               System.out.printf("b=%d is bigger",b);
+          }
+          else{
+               System.out.printf("c=%d is bigger",c);
+          }
+     }
+ }
