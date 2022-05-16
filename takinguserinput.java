825dd7666251465a6f88cab5c34261bcd9bd598d
Shaik Mohammad Sohail <mohammadsohailyt@gmail.com>
2022-05-17 00:33:15 +0530

    commit

---
+++ takinguserinput.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+class takinguserinput {
+     public static void main(String args[]) {
+          Scanner input=new Scanner(System.in);
+          System.out.println("Enter Your Name");
+          String name=input.next();
+          System.out.println("Your Name is : "+name);          
+     }
+ }
