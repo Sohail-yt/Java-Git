b2a12f7e0247a1016a3786c6ae50847eed6c7024
Shaik Mohammad Sohail <mohammadsohailyt@gmail.com>
2022-05-17 09:31:47 +0530

    commit

--- ifStatement.java
+++ ifStatement.java
@@ -1,9 +1,15 @@
 class ifStatement {
      public static void main(String args[]) {
-          int a=45;
+          int a=1145;
           int b=65;
           int c=95;
           if(a>b & a>c){
+               if(a>40){
+                    System.out.printf("%d>40\n",a);
+               }
+               else{
+                    System.out.printf("%d<40\n",a);
+               }
                System.out.printf("a=%d is bigger",a);
           }
           else if(b>a & b>c){
---
+++ switchcase.java
@@ -0,0 +1,6 @@
+class switchcase {
+     public static void main(String args[]) {
+          
+         System.out.println("Hello World!");
+     }
+ }
